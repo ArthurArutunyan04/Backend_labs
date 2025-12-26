ed1d841f1b243148283b9170e1f0b7d78b835fb5
ArthurArutunyan04 <lartur2004@yandex.ru>
2025-12-27 01:19:06 +0300

    lab4

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddScoped<RabbitMqService>();
 
 
 builder.Services.AddValidatorsFromAssemblyContaining(typeof(Program));
-builder.Services.AddScoped<ValidatorFactory>();
+builder.Services.AddScoped<ValidatorFactory>(); 
 
 builder.Services.AddControllers().AddJsonOptions(options => 
 {
